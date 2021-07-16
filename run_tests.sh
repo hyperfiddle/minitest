2a827ba1b36ee21a1900ef351bb37f202a3b3c09
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2021-07-16 18:15:42 +0200

    Cleaner tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,12 @@ echo "Clojure"
 clojure -X:test:example :dirs [\"example\"] :patterns [\"example.*\"]
 
 echo "Node"
+rm out/node-tests.js
 ./node_modules/.bin/shadow-cljs compile :test
 node out/node-tests.js
 
 echo "Browser"
+rm out/karma-tests.js
 ./node_modules/.bin/shadow-cljs compile :browser-test
 ./node_modules/.bin/karma start --single-run
 
